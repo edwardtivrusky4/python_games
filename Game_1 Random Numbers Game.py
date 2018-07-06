082ce8424dc18aaf7c9c579f0169c1f28b39892a
radikl <radikl@users.noreply.github.com>
2018-07-06 02:23:12 -0700

    Spelling error corrected in unsuccessful message.

--- Game_1 Random Numbers Game.py
+++ Game_1 Random Numbers Game.py
@@ -10,22 +10,23 @@ number = random.randint(1, 20)
 print("Well, " + my_name + ", I am thinking of a number between 1 and 20.")
 
 for guesses_taken in range(6):
-	print("Take a guess.")
-	guess = input()
-	guess = int(guess)
+    print("Take a guess.")
+    guess = input()
+    guess = int(guess)
 
-	if guess < number:
-		print("Your guess is too low.")
+    if guess < number:
+        print("Your guess is too low.")
 
-	if guess > number:
-		print("Your guess is too high.")
-	if guess == number:
-		break
+    if guess > number:
+        print("Your guess is too high.")
+    if guess == number:
+        break
 
 if guess == number:
-	guesses_taken = str(guesses_taken +1)
-	print("Good job, " + my_name + "! You guessed my number in " + guesses_taken + " guesses!")
+    guesses_taken = str(guesses_taken + 1)
+    print("Good job, " + my_name + "! You guessed my number in " +
+          guesses_taken + " guesses!")
 
 if guess != number:
-	number = str(number)
-	print("Nope. The number I waas thinkingn of was " + number + ".")
+    number = str(number)
+    print("Nope. The number I was thinking of was " + number + ".")
