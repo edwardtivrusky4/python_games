b59416fc5a6027ef061dc2bfbd96be0cf5d9b229
radikl <radikl@users.noreply.github.com>
2018-07-06 01:32:47 -0700

    GitSavvy test 1:32am

--- test.py
+++ test.py
@@ -1,4 +1,2 @@
 # This program says hello and asks for my name.
-print("Hello world!")
-print("What is your name?")
-print("Hello there")
+print("This is a test to see if GitSavvy is working properly")
